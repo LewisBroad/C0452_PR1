a125b172292f6663105e5647ccedbe380a477109
John Heatley <22135121@bucks.ac.uk>
2023-01-20 15:07:15 +0000

    started the basics for the game

--- ZombieShooterGame/ZombieShooterGame/Form1.Designer.cs
+++ ZombieShooterGame/ZombieShooterGame/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(917, 649);
+            this.Name = "Form1";
+            this.Text = "BaldyVSZombie";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
