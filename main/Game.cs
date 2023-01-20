e01071aa1291f71644cde6b13d15cb357a651bd3
Lewis <22139631@bucks.ac.uk>
2023-01-20 10:19:38 +0000

    Changed name and added another file

---
+++ main/Game.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze_Shooter_Game
+{
+    public partial class Game : game
+    {
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int enemySpeed = 3;
+        Random randNum = new Random();
+        int score;
+        
+    }
+}
